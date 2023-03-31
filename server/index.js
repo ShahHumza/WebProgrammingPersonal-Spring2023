572d3daa03175a50bb1daaed4de0950731e0fe10
Hshah101 <64723902+Hshah101@users.noreply.github.com>
2023-03-31 18:19:29 -0400

    Server Side working with Workouts

--- server/index.js
+++ server/index.js
@@ -1,49 +1,37 @@
-const express = require('express')
+const express = require('express');
 const path = require('path');
-// const jokes = require('./controllers/jokes');
-// const products = require('./controllers/products')
-const products = require('./controllers/workout')
-const app = express()
+const products = require('./controllers/workouts');
 
-let workouts = {};
-
+const app = express();
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
+let workouts = {};
+
 // Middleware
 app
-    .use(express.json())
-    .use(express.static(path.join(__dirname, '../client/dist')))
-
+  .use(express.json())
+  .use(express.static(path.join(__dirname, '../client/dist')));
 
 // Actions
 app
-    .get('/workouts', (req, res) => {
-        res.json(workouts);
-    })
-    .use('/api/v1/products', products)
-    .use('/api/v1/jokes', jokes)
+  .get('/workouts', (req, res) => {
+    res.json(workouts);
+  })
+  .post('/workouts', (req, res) => {
+    const { name, date, duration } = req.body;
+    if (!workouts[name]) {
+      workouts[name] = [];
+    }
+    workouts[name].push({ date, duration });
+    res.send('Workout added successfully!');
+  });
 
 // Catch all
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
-})
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
-app
-    .post('/workouts', (req, res) => {
-        const { name, date, duration } = req.body;
-        if (!workouts[name]) {
-        workouts[name] = [];
-        }
-        workouts[name].push({ date, duration });
-        res.send('Workout added successfully!');
-    });
-
-
-console.log('1: About to start server')
-
-app.listen(port, () => 
-  console.log(`2: Server running at http://${hostname}:${port}/`)
+app.listen(port, () =>
+  console.log(`Server running at http://${hostname}:${port}/`)
 );
-
-console.log('3: Asked server to start')
---
+++ server/models/workouts.js
@@ -0,0 +1,41 @@
+const data = require('../data/workouts.json');
+
+function getWorkouts() {
+return data.workouts;
+}
+
+function getWorkoutById(id) {
+return data.workouts.find(workout => workout.id === id);
+}
+
+function addWorkout(workout) {
+workout.id = data.workouts.length + 1;
+data.workouts.push(workout);
+}
+
+function updateWorkout(workout) {
+const index = data.workouts.findIndex(w => w.id === workout.id);
+data.workouts[index] = workout;
+}
+
+function deleteWorkout(id) {
+const index = data.workouts.findIndex(w => w.id === id);
+data.workouts.splice(index, 1);
+}
+
+function searchWorkouts(searchTerm) {
+return data.workouts.filter(workout => {
+return workout.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+workout.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
+workout.category.toLowerCase().includes(searchTerm.toLowerCase());
+});
+}
+
+module.exports = {
+getWorkouts,
+getWorkoutById,
+addWorkout,
+updateWorkout,
+deleteWorkout,
+searchWorkouts
+};
