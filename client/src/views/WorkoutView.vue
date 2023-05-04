35bfc3a50dac380989ea58d25b66b701ccfcb2d0
Hshah101 <64723902+Hshah101@users.noreply.github.com>
2023-05-04 01:27:03 -0400

    Clean Up, refractor

--- client/src/views/WorkoutView.vue
+++ client/src/views/WorkoutView.vue
@@ -1,124 +1,109 @@
 <script setup lang="ts">
-  import { onMounted, ref } from 'vue';
-  import { useSession, useUser} from '@/model/session';
-  import  { type Workout, deleteWorkout } from "@/model/workouts";
-  import { addWorkouts, getWorkouts } from "@/model/workouts";
+import { onMounted, ref } from 'vue';
+import { useSession, useUser } from '@/model/session';
+import { type Workout, deleteWorkout } from "@/model/workouts";
+import { addWorkouts, getWorkouts } from "@/model/workouts";
 
-  const session = useSession();
-  const newWorkout = ref({ date: '', duration: 0, name: '' });
-  const workouts = ref<Workout[]>([]);
+const session = useSession();
+const newWorkout = ref({ date: '', duration: 0, name: '' });
+const workouts = ref<Workout[]>([]);
 
-  const user = session.user;
-  const username = user?.name
-  // console.log(username)
-  const userId = useUser()
+const user = session.user;
+const username = user?.name
+// console.log(username)
+const userId = useUser()
 
-  async function addToWorkouts(name: string, duration: number) {
+async function addToWorkouts(name: string, duration: number) {
   await addWorkouts(userId, name, duration);
   await getUserWorkouts()
 }
 
-  async function removeWorkout(name: string, duration: number) {
-    await deleteWorkout(userId, name, duration);
-    await getUserWorkouts()
-  }
+async function removeWorkout(name: string, duration: number) {
+  await deleteWorkout(userId, name, duration);
+  await getUserWorkouts()
+}
 
-  async function getUserWorkouts() {
-    getWorkouts(userId).then((data) => {
-      workouts.value = data.data;
-      // console.log(data.data);
-      // data.data.push({ name, duration });
-    });
-  }
-  
+async function getUserWorkouts() {
+  getWorkouts(userId).then((data) => {
+    workouts.value = data.data;
+    // console.log(data.data);
+    // data.data.push({ name, duration });
+  });
+}
 
-  onMounted(async () => {
-    getUserWorkouts();""
-  })
-  
+
+onMounted(async () => {
+  getUserWorkouts(); ""
+})
+
 </script>
 
 <template>
   <main class="work">
 
     <div class="box">
-  <h1 class="title is-4">Add to Workout Log</h1>
-  <hr>
-  <form>
-    <div class="field">
-      <label class="label">Exercise</label>
-      <div class="control">
-        <input class="input" type="text" placeholder="e.g. Bench Press" v-model="newWorkout.name">
-      </div>
-    </div>
-    <!-- <div class="field">
+      <h1 class="title is-4">Add to Workout Log</h1>
+      <hr>
+      <form>
+        <div class="field">
+          <label class="label">Exercise</label>
+          <div class="control">
+            <input class="input" type="text" placeholder="e.g. Bench Press" v-model="newWorkout.name">
+          </div>
+        </div>
+        <!-- <div class="field">
       <label class="label">Weight</label>
       <div class="control">
         <input class="input" type="text" placeholder="e.g. 135 lbs">
       </div>
     </div> -->
-    <div class="field">
-      <label class="label">Duration</label>
-      <div class="control">
-        <input class="input" type="text" placeholder="e.g. 10" v-model="newWorkout.duration">
-      </div>
-    </div>
-    <div class="field">
-      <label class="label">Date</label>
-      <div class="control">
-        <input class="input" type="date">
-      </div>
-    </div>
-    <div class="field">
-      <label class="label">Notes</label>
-      <div class="control">
-        <textarea class="textarea" placeholder="e.g. Felt great today"></textarea>
-      </div>
-    </div>
-    <div class="field">
-      <div>
-        <button type="button" class="button is-primary" @click="addToWorkouts(newWorkout.name, newWorkout.duration)">Add</button>
-      </div>
-      <!-- <div class="control">
+        <div class="field">
+          <label class="label">Duration</label>
+          <div class="control">
+            <input class="input" type="text" placeholder="e.g. 10" v-model="newWorkout.duration">
+          </div>
+        </div>
+        <div class="field">
+          <label class="label">Date</label>
+          <div class="control">
+            <input class="input" type="date">
+          </div>
+        </div>
+        <div class="field">
+          <label class="label">Notes</label>
+          <div class="control">
+            <textarea class="textarea" placeholder="e.g. Felt great today"></textarea>
+          </div>
+        </div>
+        <div class="field">
+          <div>
+            <button type="button" class="button is-primary"
+              @click="addToWorkouts(newWorkout.name, newWorkout.duration)">Add</button>
+          </div>
+          <!-- <div class="control">
         <button class="button is-link">Cancel</button>
       </div> -->
+        </div>
+      </form>
     </div>
-  </form>
-</div>
 
 
     <h1 class="title">
-      <!-- <div>
-        <label>Workout Name:</label>
-        <input type="text" v-model="newWorkout.name">
-      </div>
-      <div>
-        <label>Workout Duration(minutes):</label>
-        <input type="text" v-model="newWorkout.duration">
-      </div>
-      <div>
-            <label>Workout Date:</label>
-            <input type="text" v-model="newWorkout.date">
-          </div> 
-          <button @click="addToWorkouts(newWorkout.name, newWorkout.duration)">Add Workout</button>
-      <br>-->
       <ul>
         <li v-for="(workout, index) in workouts" :key="workout.id">
           <div class="notification is-primary">
-            <button class="delete" @click="removeWorkout(workout.name, workout.duration) "></button>
-            {{ workout.name }} - {{ workout.duration }}
+            <button class="delete" @click="removeWorkout(workout.name, workout.duration)"></button>
+            {{ workout.name }} - {{ workout.duration }} -{{ workout.date }}
           </div>
         </li>
       </ul>
-      
+
     </h1>
   </main>
 </template>
 
 
 <style>
-
-
 @media (min-width: 512px) {
 
   .work,
@@ -129,16 +114,20 @@
     min-height: 100vh;
     display: flex;
     text-align: right;
-    margin-top: -350px;
-    z-index: 0;
-    
-    
+    margin-top: 50px;
+    /* z-index: 0; */
+
+
 
     /* This alligns it */
     position: relative;
     width: calc(100% - 280px);
-    left: 280px;
+    left: 100px;
     transition: .3s ease;
+
+
+    
   }
 }
+
 </style>
